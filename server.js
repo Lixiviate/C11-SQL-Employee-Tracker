0a5332c81230f7c23aa4c9fd12dc7127e6839ff8
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-07-30 00:05:35 -0600

    Update server.js with connection to postgres

--- server.js
+++ server.js
@@ -1,11 +1,37 @@
 const express = require("express");
-const path = require("path");
-
 const { Pool } = require("pg");
+const { database } = require("pg/lib/defaults");
+
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 // Middleware for the parsing of JSON data
 app.use(express.json());
 // Middleware for parsing of URL encoded data
 // Does this need to be false?
 app.use(express.urlencoded({ extended: false }));
+
+// Access org_db
+const pool = new Pool(
+  {
+    user: "********",
+    password: "*********",
+    host: "127.0.0.1",
+    database: "********",
+  },
+  console.log("Access to org_db established.")
+);
+
+pool.connect();
+
+pool.query("SELECT * FROM department", function (err, { rows }) {
+  console.log(rows);
+});
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
