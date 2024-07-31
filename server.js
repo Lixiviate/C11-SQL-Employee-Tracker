e1fdb0ebfe1bd63c19741a94043fb72e893f8700
Lixiviate <98005291+Lixiviate@users.noreply.github.com>
2024-07-30 22:15:40 -0600

    Update package info, add inquirer prompt and view all dept function

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const inquirer = require("inquirer");
 const { Pool } = require("pg");
 const { database } = require("pg/lib/defaults");
 
@@ -14,19 +15,66 @@ app.use(express.urlencoded({ extended: false }));
 // Access org_db
 const pool = new Pool(
   {
-    user: "********",
-    password: "*********",
+    user: "postgres",
+    password: "SQLpss",
     host: "127.0.0.1",
-    database: "********",
+    database: "org_db",
   },
   console.log("Access to org_db established.")
 );
 
+const mainMenu = () => {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "selection",
+      message: "Please select an option:",
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update an employee role",
+        "Exit",
+      ],
+    })
+    .then((answer) => {
+      switch (answer.selection) {
+        case "View all departments":
+          viewAllDept();
+          break;
+        case "View all roles":
+          break;
+        case "View all employees":
+          break;
+        case "Add a department":
+          break;
+        case "Add a role":
+          break;
+        case "Add an employee":
+          break;
+        case "Update an employee role":
+          break;
+        case "Exit":
+          pool.end();
+          break;
+      }
+    });
+};
+
 pool.connect();
 
-pool.query("SELECT * FROM department", function (err, { rows }) {
-  console.log(rows);
-});
+const viewAllDept = () => {
+  pool.query("SELECT * FROM department", function (err, res) {
+    console.log(res.rows);
+    mainMenu();
+  });
+};
+
+// Start the app
+mainMenu();
 
 app.use((req, res) => {
   res.status(404).end();
